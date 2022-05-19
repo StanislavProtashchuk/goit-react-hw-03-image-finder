2b7e2ff0361379d4b4a1e1872ea541f03aa53a78
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-05-19 13:58:08 +0300

    everythingBad

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,25 +1,43 @@
 import { Component } from 'react';
+import { toast } from 'react-toastify';
 
 export default class Searchbar extends Component{
     state = {
-        cats: '',
-    }
+        name: '',
+    };
+
+    handleNameChange = e => {
+        this.setState({ name: e.currentTarget.value.toLowerCase() });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        if (this.state.name.trim() === '') {
+            return toast.error('Enter picture name!');
+            // return;
+        }
+        this.props.onSubmit(this.state.name);
+        this.setState({ name: '' });
+    };
 
 
 render(){
     return(
         <header class= "searchbar" >
-        <form class="form">
+        <form class="form" onSubmit={this.handleSubmit}>
             <button type="submit" class="button">
                 <span class="button-label">Search</span>
-            </button>
-            
-            <input
-                class="input"
-                type="text"
-                autocomplete="off"
-                autofocus
-                placeholder="Search images and photos"
+                </button>
+                
+                <input
+                    class="input"
+                    type="text"
+                    autocomplete="off"
+                    autofocus
+                    placeholder="Search images and photos"
+                    value={this.state.name}
+                    onChange={this.handleNameChange}
             />
         </form>
         </header>
