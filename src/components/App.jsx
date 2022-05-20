fed328157bbfa899b24bd9041f9035b4f31cf8ad
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-05-20 15:22:45 +0300

    noRender

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css';
+import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
 import Searchbar from "./Searchbar/Searchbar";
 
 export default class App extends Component {
@@ -13,20 +15,6 @@ export default class App extends Component {
     console.log(name);
   };
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// const KEY: '25738191-2a68a887d2690264ace752ae1';
-
-  // componentDidMount() {    
-  //   this.setState({ loading: true })
-  //   fetch(`https://pixabay.com/api/?key=25738191-2a68a887d2690264ace752ae1&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=1`)
-  //     .then(res => res.json())
-  //     .then(cat => this.setState({ cat }))
-  //     .finally(() => this.setState({ loading: false }))
-    
-  // }
-  
-
-
   render() {
 
     // const { cats } = this.state;
@@ -34,7 +22,8 @@ export default class App extends Component {
     return (
       <div>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ToastContainer autoClose={2000} position="top-center" />
+        <ToastContainer autoClose={3000} />
+        <ImageGalleryItem name={this.state.name}/>
         {/* {this.state.loading && <h1>Loading</h1>}
         {(
           <ul>
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,3 +1,41 @@
-<li class="gallery-item">
-  <img src="" alt="" />
-</li>
+import { Component } from 'react';
+
+export default class ImageGalleryItem extends Component {
+  state = {
+    pictures: []
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.name;
+    const nextName = this.props.name;
+
+    if (prevName !== nextName) {
+      console.log('Змінилось імя', nextName);
+
+      this.setState({ loading: true });
+      fetch(`https://pixabay.com/api/?key=25738191-2a68a887d2690264ace752ae1&q=${nextName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=1`)
+        .then(res => res.json())
+        .then(pictures => this.setState({ pictures }))
+        .finally(() => this.setState({ loading: false }));
+      
+    }
+  }
+
+  render() {
+    const { pictures, loading } = this.state
+    const { id, webformatURL } = pictures
+      return (
+        <div>
+          {loading && <div>Loading...</div>}
+          {pictures &&
+            <ul>
+              <li id={id} class="gallery-item">
+                <img src={webformatURL} alt="" />
+              </li>
+            </ul >
+          }
+        </div>
+      )
+
+  }
+}
