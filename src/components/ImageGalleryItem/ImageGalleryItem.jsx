55c1347953f3258c57295fef199e362253445908
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-05-22 14:13:03 +0300

    fetch working

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,45 +1,58 @@
 import { Component } from 'react';
-import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export default class ImageGalleryItem extends Component {
   state = {
-    pictures: [],
-    loading: false,
+    pictures: null,
+    error: null,
+    status: 'idle' ,
   }
 
   componentDidUpdate(prevProps, prevState) {
     const prevName = prevProps.name;
     const nextName = this.props.name;
 
-    if (prevName !== nextName) {
-      console.log('Змінилось імя', nextName);
+      if (prevName !== nextName) {
+      this.setState({status: 'pending'});
+        fetch(`https://pixabay.com/api/?key=25738191-2a68a887d2690264ace752ae1&q=${nextName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=1`)
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
 
-      this.setState({ loading: true, pictures: [] });
-      axios.get(`https://pixabay.com/api/?key=25738191-2a68a887d2690264ace752ae1&q=${nextName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=1`)
-        .then(res => {
-          return res.data.hits;
-        })
-        .then(pictures => this.setState({ pictures }))
-        .finally(() => this.setState({ loading: false }));
-      
+            return Promise.reject(
+              new Error(`No picture with name ${nextName}`),
+            );
+          })
+          .then(pictures => this.setState({ pictures, status: 'resolved' }))
+          .catch(error => this.setState({ error, status: 'rejected' }))
     }
   }
 
   render() {
-    const { pictures, loading} = this.state
-    return pictures.map(({ id, webformatURL }) => {
-      return (
-        <div>
-          {loading && <div>Loading...</div>}
-          {pictures &&
-            <ul>
-              <li id={id} class="gallery-item">
-                <img src={webformatURL} alt="" />
-              </li>
-            </ul >
-          }
-        </div>
-      )
-    })
+    const { pictures, status} = this.state
+    
+    if (status === 'idle') {
+  return <div>Please enter picture name.</div>
+    }
+    
+    if (status === 'pending') {
+      return <div>Loading...</div>
+    }
+
+    if (status === 'rejected' || pictures.hits.length === 0) {
+      // return <h1>No picture with name: "{this.props.name}"</h1>
+      return toast.error(`No picture with name: "${this.props.name}"`);
+    }
+
+    if (status === 'resolved') {
+      return pictures.hits.map(({ id, webformatURL }) => {
+        return <ul>
+          <li id={id} class="gallery-item">
+            <img src={webformatURL} alt="" />
+          </li>
+        </ul >
+      })
+    }
   }
 }
